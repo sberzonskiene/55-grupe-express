cca91464c8b3ba7dc01e21b342ae029620982014
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-12 11:45:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send(new PageHome().render()));
-app.get('/', (req, res) => res.send(new PageAbout().render()));
-app.get('/', (req, res) => res.send(new PageRegister().render()));
-app.get('/', (req, res) => res.send(new Page404().render()));
+app.get('/about', (req, res) => res.send(new PageAbout().render()));
+app.get('/register', (req, res) => res.send(new PageRegister().render()));
+app.get('/*error', (req, res) => res.send(new Page404().render()));
 
 app.listen(port, () => {
   console.log(`Server is running on: http://localhost:${port}`);

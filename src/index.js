fbffbb632f4192efb6530ac97e60110fb624ae03
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-16 10:11:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PageHome } from './pages/PageHome.js';
 import { PageAbout } from './pages/PageAbout.js';
+import { PageServices } from './pages/PagesServices.js';
 import { PageRegister } from './pages/PageRegister.js';
 import { Page404 } from './pages/Page404.js';
 
@@ -11,6 +12,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send(new PageHome().render()));
 app.get('/about', (req, res) => res.send(new PageAbout().render()));
+app.get('/services', (req, res) => res.send(new PageServices().render()));
 app.get('/register', (req, res) => res.send(new PageRegister().render()));
 app.get('*error', (req, res) => res.send(new Page404().render()));
 

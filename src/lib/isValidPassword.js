14ae4a18b4d71bf28e95a3632063b6c64c08377d
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-18 09:51:01 +0300

    update

--- src/lib/isValidPassword.js
+++ src/lib/isValidPassword.js
@@ -1,17 +1,17 @@
 export function isValidPassword(text) {
     const minSize = 12;
     const maxSize = 100; 
-    
+
     if (typeof text !== 'string') {
         return [true, 'Password turi buti tekstas'];
     }
 
     if (text.length < minSize) {
-        return [true, 'Password turi tureti ne maziau 12 simboliu'];
+        return [true, `Password turi tureti ne maziau ${minSize} simboliu`];
     }
 
     if (text.length > maxSize) {
-        return [true, 'Password turi tereti ne daugiau 100 simboliu'];
+        return [true, `Password turi tereti ne daugiau ${maxSize} simboliu`];
     }
 
     return [false, ''];
